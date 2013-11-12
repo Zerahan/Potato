748c069cec93b952859c45eefe65ab6432e70cfd
Zerahan <Zerahan01@gmail.com>
2013-11-12 08:14:09 -0600

    v1.0 - Wind Tunnel (broken)

---
+++ Assets/Scripts/Object/WindTunnel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindTunnel : MonoBehaviour{
+	public Vector3 windDirection	= new Vector3(0,0,0);
+	public float windStrength		= 5f;
+	
+	private Vector3 wind;
+	
+	public void Start(){
+		wind = windDirection.normalized * windStrength;
+	}
+	
+	public void OnTriggerStay(Collider collider){
+		collider.rigidbody.AddForce(wind);
+	}
+	
+	public void OnDrawGizmos(){
+		Gizmos.color	= new Color(1,1,1,0.25f);
+		Gizmos.DrawCube(transform.position,transform.localScale);
+		Gizmos.color	= Color.yellow;
+		Gizmos.DrawWireCube(transform.position,transform.localScale);
+		Gizmos.DrawRay(transform.position,windDirection*windStrength);
+	}
+}
