250e1a508dc0da57c8d8a21f5c1869a96d4686e4
Zerahan <Zerahan01@gmail.com>
2013-11-12 02:48:09 -0600

    v1.0 - Minor Script Changes

--- Assets/Scripts/Object/BreakableObject.cs
+++ Assets/Scripts/Object/BreakableObject.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class BreakableObject : MonoBehaviour {
+	public float breakSpeed	= 15;
+	
 	public ParticleSystem particle;
 	
 	public void OnCollisionEnter( Collision collision ){
-		if( collision.relativeVelocity.magnitude > 15 ){
+		if( collision.relativeVelocity.magnitude > breakSpeed ){
 			ParticleSystem p = (ParticleSystem)Instantiate(particle,transform.position,Quaternion.identity);
 			Destroy(gameObject);
 		}
