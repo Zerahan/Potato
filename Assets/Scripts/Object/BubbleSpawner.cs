fb3e690a8e62605db263deb344ae4824723fc8b7
Thalassicus <Thalassicus@hotmail.com>
2013-11-13 06:12:14 -0600

    v1 - bubble and art updates

--- Assets/Scripts/Object/BubbleSpawner.cs
+++ Assets/Scripts/Object/BubbleSpawner.cs
@@ -5,7 +5,9 @@ public class BubbleSpawner : MonoBehaviour {
 	
 	public float spawnRate	= 0.5f;
 	public int maxSpawns	= 10;
-	public float lifeSpan	= 10;
+	public float minAge	= 2f;
+	public float maxAge	= 30f;
+	
 	
 	public GameObject bubble;
 	
@@ -14,22 +16,30 @@ public class BubbleSpawner : MonoBehaviour {
 	private bool spawned;
 	private float timer;
 	private int index;
+	private float lifespan;
+	private float ageRange;
 	
 	void Start(){
-		timer	= Random.Range(0.0f,1.0f);
-		bubbles = new GameObject[maxSpawns];
+		timer		= Random.Range(spawnRate * 0.67f,spawnRate * 1.5f);
+		bubbles 	= new GameObject[maxSpawns];
+		ageRange	= maxAge - minAge;
 	}
 	
 	void Update () {
 		if( timer < Time.time ){
-			timer	= Time.time + Random.Range(0.1f,spawnRate);
+			timer	= Time.time + Random.Range(spawnRate * 0.67f,spawnRate * 1.5f);
 			index	= 0;
 			spawned	= false;
 			while(!spawned && index < maxSpawns){
 				if( !bubbles[index] ){
 					spawned = true;
+					
+					lifespan = Random.Range(minAge,maxAge);
+					lifespan = Mathf.Pow((lifespan - minAge) / ageRange, 2);
+					lifespan = minAge + ageRange * lifespan;
+					
 					bubbles[index] = (GameObject)Instantiate(bubble,new Vector3(transform.position.x,transform.position.y,transform.position.z),Quaternion.identity);
-					bubbles[index].GetComponent<Bubble>().SetLifeSpan(lifeSpan);
+					bubbles[index].GetComponent<Bubble>().maxAge = lifespan;
 					bubbles[index].transform.localEulerAngles = new Vector3(0.0f,0.0f,Random.Range(0.0f,360.0f));
 				}
 				index++;
--- Assets/Scripts/Player/Equipment/Boost.cs
+++ Assets/Scripts/Player/Equipment/Boost.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-//tempchange <- remove this line
 
 public class Boost : MonoBehaviour {
 	private string	BUTTON				= "Boost";
@@ -9,6 +8,9 @@ public class Boost : MonoBehaviour {
 	public int 		maxBoosts			= 10;
 	public float	boostRestoreDelay	= 10;
 	public float 	moveStrength		= 1f;
+	public GameObject body				;
+	public Light	light				;
+	public float	maxLightIntensity	= 4;
 	
 	private UserInput userInput;
 	private bool 	isDebug 		= false;
@@ -25,10 +27,10 @@ public class Boost : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start(){
-		userInput	= GetComponent<UserInput>();
-		boostStrength = boostStrength * Physics.gravity.magnitude;
-		moveStrength = moveStrength * Physics.gravity.magnitude;
-		maxEnergy	= boostStrength * maxBoosts;
+		userInput		= GetComponent<UserInput>();
+		boostStrength	= boostStrength * Physics.gravity.magnitude;
+		moveStrength	= moveStrength * Physics.gravity.magnitude;
+		maxEnergy		= boostStrength * maxBoosts;
 		if (isDebug) {
 			energy = 500f;
 			rigidbody.useGravity = false;
@@ -37,61 +39,32 @@ public class Boost : MonoBehaviour {
 		}
 	}
 	
+	
 	void Update(){
-		if(energy >= boostStrength){
-			GetComponent<Light>().intensity = (3 * (energy/maxEnergy))+1f;
-		}else{
-			GetComponent<Light>().intensity = 0f;
-		}
+		light.intensity = Mathf.Sqrt(Mathf.Floor(energy/boostStrength)/maxBoosts) * maxLightIntensity;
 	}
 	
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		moveDirection	= (userInput.GetMousePosition() - transform.position).normalized;
-		//moveDirection.x = Input.GetAxis("Horizontal");
-		//moveDirection.y = Input.GetAxis("Vertical");
 		useGravity		= true;
-		//lastButtonState	= buttonState;
-		//buttonState		= Input.GetButton(button);//moveDirection.magnitude > 0;
 		
 		if(energy > 0 && moveDirection.magnitude > 0){
 			
-			// first frame of button press - instant boost
-			//if( !lastButtonState && buttonState && energy >= boostStrength ){
 			if( Input.GetButtonDown(BUTTON) && energy >= boostStrength ){
 				rigidbody.velocity += moveDirection.normalized * boostStrength;
 				if (!isDebug) {
 					energy -= boostStrength;
 				}
 			}
-			
-			/*
-			// later frames of button press - gradual force
-			if( lastButtonState && buttonState ){
-				useGravity	= false;
-				if(	energy - moveStrength > 0 ){
-					energy -= moveStrength;
-					rigidbody.AddForce(moveDirection.normalized * moveStrength);
-				}else{
-					Debug.Log("Expending all energy...");
-					rigidbody.AddForce(moveDirection.normalized * energy);
-					energy = 0;
-				}
-			}
-			*/
 		}
 		if (energy < maxEnergy) {
 			energy = Mathf.Min(maxEnergy, energy + (Time.deltaTime * boostStrength) / boostRestoreDelay );
 		}
-		
-		
-		
-		//rigidbody.useGravity = useGravity;
 	}
 	
 	public void OnCollisionEnter(Collision collision){
-		//Debug.Log("Adding Energy: " + (collision.relativeVelocity.magnitude));
 		if (!isDebug) {
 			energy = Mathf.Min(maxEnergy, energy + Mathf.Min(boostStrength,collision.relativeVelocity.magnitude));
 		}
