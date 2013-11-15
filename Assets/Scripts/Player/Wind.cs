6d332cad59e6e4930fcb8be545854b65fd7275f5
Thalassicus <Thalassicus@hotmail.com>
2013-11-15 10:24:33 -0600

    v1 - player movement

--- Assets/Scripts/Player/Wind.cs
+++ Assets/Scripts/Player/Wind.cs
@@ -9,7 +9,7 @@ public class Wind : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		sound	= Camera.main.GetComponent<AudioSource>();
+		sound	= rigidbody.GetComponent<AudioSource>();
 		sound.clip		= audio;
 		sound.volume	= 0;
 		sound.pitch		= 0.8f;
@@ -18,13 +18,11 @@ public class Wind : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//*
 		velocity = rigidbody.velocity.magnitude;
-		if (velocity > 0.5) {
-			sound.volume = rigidbody.velocity.magnitude / 20;
+		if (velocity > 1) {
+			sound.volume = (rigidbody.velocity.magnitude - 5) / 500;
 		}else{
 			sound.volume = 0;
 		}
-		//*/
 	}
 }
